65a218b6b98748fc923aa245ebb4ce46dbb77e1f
ilyaprogger <ilyaprogger>
2019-04-24 17:33:19 +0300

    a

--- src/test/java/Pack/PackRLETest.java
+++ src/test/java/Pack/PackRLETest.java
@@ -26,16 +26,16 @@ public class PackRLETest {
     }
 
     @Test
-    void test2() throws Exception{
-        DataInputStream input = new DataInputStream(new FileInputStream("text1"));
-        DataOutputStream output = new DataOutputStream(new FileOutputStream("RlEtext1"));
-        DataInputStream input1 = new DataInputStream(new FileInputStream("RlEtext1"));
-        DataOutputStream output1 = new DataOutputStream(new FileOutputStream("atext1"));
+    void test3() throws Exception {
+        DataInputStream input = new DataInputStream(new FileInputStream("text2"));
+        DataOutputStream output = new DataOutputStream(new FileOutputStream("RlEtext2"));
+        DataInputStream input1 = new DataInputStream(new FileInputStream("RlEtext2"));
+        DataOutputStream output1 = new DataOutputStream(new FileOutputStream("atext2"));
         PackRLE pack = new PackRLE();
         pack.rle(input,output);
         pack.antiRle(input1,output1);
-        File file = new File("text1");
-        File file1 = new File("atext1");
+        File file = new File("text2");
+        File file1 = new File("atext2");
         assertTrue(FileUtils.contentEquals(file,file1));
         output.close();
         input.close();
